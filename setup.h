34a1981765dbd51f2bf2acbfb02269832adb0e44
ELİTE <efe113878@gmail.com>
2026-05-18 21:51:49 +0300

    Update setup.h

--- setup.h
+++ setup.h
@@ -3,27 +3,25 @@
 
 #include <stdint.h>
 
-// Kurulum Kademeleri - Tek bir yerde tanımlı
 typedef enum {
-    STAGE_WIND_COUNTRY = 0,    // "Bu doğru ülke mi?" Adımı
-    STAGE_WIND_KEYBOARD,       // "Klavye Düzeni" Adımı
-    STAGE_WIND_NETWORK,        // "Ağ Bağlantısı" Adımı
-    STAGE_WIND_HOSTNAME,       // "Wind OS istasyonunuza bir ad verelim"
-    STAGE_WIND_CUSTOMIZE,      // "Wind OS deneyiminizi özelleştirin"
+    STAGE_WIND_COUNTRY = 0,    
+    STAGE_WIND_KEYBOARD,       
+    STAGE_WIND_NETWORK,        
+    STAGE_WIND_HOSTNAME,       
+    STAGE_WIND_CUSTOMIZE,      
     STAGE_WIND_COMPLETE
 } SetupStage;
 
 #define SCREEN_WIDTH  320
 #define SCREEN_HEIGHT 200
 
-// Renk Paleti İndeksleri
-#define COLOR_WIND_BG        16  // Nötr Gri Arka Plan
-#define COLOR_WIND_CARD      17  // Saf Beyaz Panel
-#define COLOR_WIND_TEXT      18  // Koyu Lacivert/Siyah Yazı
-#define COLOR_WIND_PRIMARY   19  // Windland OS Mavisi (Butonlar)
-#define COLOR_WIND_SWIRL     20  // Fırtına Turkuazı (Wind Efektleri)
-#define COLOR_WIND_3D_LIGHT  21  // Parlayan Kenar Beyazı
-#define COLOR_WIND_3D_DARK   22  // Derin Gölge Koyu Gri
+#define COLOR_WIND_BG        16  
+#define COLOR_WIND_CARD      17  
+#define COLOR_WIND_TEXT      18  
+#define COLOR_WIND_PRIMARY   19  
+#define COLOR_WIND_SWIRL     20  
+#define COLOR_WIND_3D_LIGHT  21  
+#define COLOR_WIND_3D_DARK   22  
 
 void setup_init(void);
 void setup_render(void);
