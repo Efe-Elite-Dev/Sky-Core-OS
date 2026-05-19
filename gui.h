6cfdfdbda2c3d6ca1de992a2a44a65a9aa8b9e47
ELİTE <efe113878@gmail.com>
2026-05-19 19:48:27 +0300

    Update gui.h

--- gui.h
+++ gui.h
@@ -1,10 +1,6 @@
 #ifndef GUI_H
 #define GUI_H
+#include "globals.h"
 
-#include <stdint.h>
-
-void draw_rect_pure(int x, int y, int w, int h, uint8_t color);
 void draw_window_pure(int x, int y, int w, int h, const char* title);
-void gui_init(void);
-
 #endif
