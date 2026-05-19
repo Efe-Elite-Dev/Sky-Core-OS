e6a71bf51816b17e7e2e6089c5e6da7bcc735f7a
ELİTE <efe113878@gmail.com>
2026-05-19 20:58:20 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,32 +1,42 @@
-; boot.asm - Wind OS / Sky Core OS Bootloader Giriş Noktası
-MBOOT_PAGE_ALIGN    equ 1 << 0
-MBOOT_MEM_INFO      equ 1 << 1
-MBOOT_GRAPHICS      equ 1 << 2  ; Grafik modu için gerekli flag
-MBOOT_HEADER_MAGIC  equ 0x1BADB002
-MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO | MBOOT_GRAPHICS
-MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+; boot.asm
+MULTIBOOT_ALIGN     equ 1 << 0
+MULTIBOOT_MEMINFO   equ 1 << 1
+MULTIBOOT_GRAPHICS  equ 1 << 2  ; GRUB'a grafik modu aç diyoruz
+MULTIBOOT_FLAGS     equ MULTIBOOT_ALIGN | MULTIBOOT_MEMINFO | MULTIBOOT_GRAPHICS
+MULTIBOOT_MAGIC     equ 0x1BADB002
+MULTIBOOT_CHECKSUM  equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
 
 section .multiboot
 align 4
-    dd MBOOT_HEADER_MAGIC
-    dd MBOOT_HEADER_FLAGS
-    dd MBOOT_CHECKSUM
-    ; VBE Modu İsteği
-    dd 0, 0, 0, 0, 0
-    dd 0          ; Mode type (0 = Linear)
-    dd 1024       ; Width
-    dd 768        ; Height
-    dd 32         ; Depth
+    dd MULTIBOOT_MAGIC
+    dd MULTIBOOT_FLAGS
+    dd MULTIBOOT_CHECKSUM
+    ; Grafik Ekran İstek Ayarları
+    dd 0
+    dd 800  ; Genişlik
+    dd 600  ; Yükseklik
+    dd 32   ; Renk Derinliği (BPP)
 
 section .text
 global _start
 extern kernel_main
 
 _start:
-    push eax      ; Magic number'ı kernel'a yolla
-    push ebx      ; Multiboot header adresini yolla
-    call kernel_main
     cli
-.hang:
+    mov esp, stack_top
+    
+    ; GRUB'ın bize verdiği sistem ve ekran bilgilerini C çekirdeğine paslıyoruz
+    push ebx         ; Multiboot bilgi yapısının adresi
+    push eax         ; Sihirli numara (Magic Number)
+    
+    call kernel_main
+    
+.halt:
     hlt
-    jmp .hang
+    jmp .halt
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384       ; 16KB Güvenli Stack Alanı
+stack_top:
